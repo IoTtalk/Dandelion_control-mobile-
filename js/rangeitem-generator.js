f86c0b98141bb58cb12bc5239138ed301d0ae46f
jerryliu0811 <jerryliu0811@gmail.com>
2018-08-15 00:12:54 +0800

    add default value & set correct min max value

--- js/rangeitem-generator.js
+++ js/rangeitem-generator.js
@@ -1,20 +1,20 @@
 $(function() {
     var feature_level_data = {
-        //'feature_name' : ['min', 'max', 'step']
-        'size':         [1, 10, 1],
-        'angle':        [0-360, 360, 72],
-        'color_r':      [0, 255, 25],
-        'color_g':      [0, 255, 25],
-        'color_b':      [0, 255, 25],
-        'bg_color_r':   [0, 255, 25],
-        'bg_color_g':   [0, 255, 25],
-        'bg_color_b':   [0, 255, 25],
-        's0':           [0, 960, 96],
-        's1':           [3, 38, 3],
-        's2':           [0, 123, 12],
-        's3':           [0, 376, 37],
-        'a1':           [0, 359, 35],
-        'easingRate' :  [0.01, 1, 0.1]
+        //'feature_name' : ['min', 'max', 'default', 'step']
+        'size':         [0, 10, 0, 1],
+        'angle':        [0-360, 360, 0, 72],
+        'color_r':      [0, 255, 255, 25.5],
+        'color_g':      [0, 255, 255, 25.5],
+        'color_b':      [0, 255, 255, 25.5],
+        'bg_color_r':   [0, 255, 0, 25.5],
+        'bg_color_g':   [0, 255, 0, 25.5],
+        'bg_color_b':   [0, 255, 0, 25.5],
+        's0':           [192, 476, 384, 28.4],
+        's1':           [1, 19, 10, 1.8],
+        's2':           [0, 60, 6, 6],
+        's3':           [0, 60, 3, 6],
+        'a1':           [0, 360, 120, 36],
+        'easingRate' :  [0.01, 1, 0.1, 0.099]
     }
 
     var min, max, step, default_val;
@@ -22,8 +22,8 @@ $(function() {
     for(var feature in feature_level_data) {
         min = feature_level_data[feature][0];
         max = feature_level_data[feature][1];
-        step = feature_level_data[feature][2];
-        default_val = min + step * 5;
+        default_val = feature_level_data[feature][2];
+        step = feature_level_data[feature][3];
         rangeitem_str = 
             "<tr>\
               <td width='20%'>\
@@ -35,7 +35,7 @@ $(function() {
               <td width='70%'>\
                 <form action='#'>\
                   <p class='range-field'>\
-                    <input type='range' id='" + feature + "-slider' min='" + min + "' max='" + max + "' step='" + step + "' />\
+                    <input type='range' id='" + feature + "-slider' min='" + min + "' max='" + max + "' step='" + step + "' value='" + default_val + "'/>\
                   </p>\
                 </form>\
               </td>\
