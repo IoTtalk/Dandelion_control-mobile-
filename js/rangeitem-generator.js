c382670adf389cfdc86895d5afb87155599e5b5f
jerryliu0811 <jerryliu0811@gmail.com>
2018-09-06 14:29:07 +0800

    simple version for laptop

--- js/rangeitem-generator.js
+++ js/rangeitem-generator.js
@@ -2,8 +2,8 @@ $(function() {
     var feature_level_data = {
         //'feature_name' : ['min', 'max', 'default', 'step']
         'size':         [0, 10, 0, 1],
-        'angle':        [0-360, 360, 0, 72],
-        'color_r':      [0, 255, 255, 25.5],
+        'angle':        [0, 120, 0, 10]
+        /*'color_r':      [0, 255, 255, 25.5],
         'color_g':      [0, 255, 255, 25.5],
         'color_b':      [0, 255, 255, 25.5],
         'bg_color_r':   [0, 255, 0, 25.5],
@@ -14,7 +14,7 @@ $(function() {
         's2':           [0, 60, 6, 6],
         's3':           [0, 60, 3, 6],
         'a1':           [0, 360, 120, 36],
-        'easingRate' :  [0.01, 1, 0.1, 0.099]
+        'easingRate' :  [0.01, 1, 0.1, 0.099]*/
     }
 
     var min, max, step, default_val;
@@ -43,4 +43,28 @@ $(function() {
         $('#range_items').append(rangeitem_str);
     }
     
+
+    //for special color-btn
+    min = 0;
+    max = 6;
+    default_val = "Red";
+    step = 1;
+    rangeitem_str = 
+        "<tr>\
+          <td width='15%'>\
+            <span class='name'>color</span>\
+          </td>\
+          <td class='td_val' width='15%'>\
+            <span id='color_val'>" + default_val + "</span>\
+          </td>\
+          <td width='70%'>\
+            <form action='#'>\
+              <p class='range-field'>\
+                <input type='range' id='color-slider' min='" + min + "' max='" + max + "' step='" + step + "' value='0'/>\
+              </p>\
+            </form>\
+          </td>\
+        </tr>";
+    $('#range_items').append(rangeitem_str);
+
 });
